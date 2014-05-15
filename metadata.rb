0bb84342c4ccc752b189b5fe5c5acd5f25f3d29b
Benjamin Clark <ben@benclark.com>
2014-05-15 11:20:13 -0400

    Version bump to v0.2.0 now that Redhat platform family is supported.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,8 @@
 name             "avahi"
-maintainer       "Cameron Johnston"
-maintainer_email "cameron@needle.com"
+maintainer       "Benjamin Clark"
+maintainer_email "ben@benclark.com"
 license          "Apache 2.0"
 description      "Installs a zeroconf mDNS daemon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
-supports         "ubuntu"
+version          "0.2.0"
 depends          "git"
